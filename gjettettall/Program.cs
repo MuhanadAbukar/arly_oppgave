57e36af479744a108c0c4961978b93c52d2ae428
Hanad <muhanadisnotabebe@gmail.com>
2022-11-17 13:06:11 +0100

    sd

--- gjettettall/Program.cs
+++ gjettettall/Program.cs
@@ -1,32 +1,4 @@
-﻿/*
- 
-# Program for å gjette på et tall
-
-import random
-
-Console.WriteLine()
-Console.WriteLine("Så skal du gjette på hvilket tall programmet tenker på!")
-Console.WriteLine("Programmet vil si fra om tallet er for høyt, for lavt eller rett.")
-topp = int(input("Skriv inn øvre grense for hvilket tall programmet skal tenke på."))
-Console.WriteLine("Da gjetter programmet på et tall mellom 0 og", topp, ".")
-rett = False
-tall = random.randint(0, topp)
-gjettinger = 0
-
-while rett == False:
-  gjett=int(input("Skriv inn hvilket tall du tror programmet tenker på."))
-  gjettinger = gjettinger + 1
-  if gjett < tall:
-    Console.WriteLine("Tallet du skrev inn, er for lavt.")
-  elif gjett > tall:
-    Console.WriteLine("Tallet du skrev inn, er for høyt.")
-  else:
-    Console.WriteLine("Du gjettet helt rett!")
-    rett = True
-    
-Console.WriteLine(f"Du brukte {gjettinger} gjettinger på å komme fram til rett svar.")
- */
-var Rand = new Random();
+﻿var Rand = new Random();
 Console.WriteLine("Dette programmet tenker på et tall mellom 0 og et tall du velger.");
 Console.WriteLine("Programmet vil si fra om tallet er for høyt, for lavt eller rett.");
 Console.WriteLine("Skriv inn øvre grense for hvilket tall programmet skal tenke på.");
